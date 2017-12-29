44a158c0a5e62bf90792128b931723a18312635e
Wilkware <heiko.wilknitz@wilkware.de>
2017-12-29 22:53:28 +0100

    First try

---
+++ Almanac/module.php
@@ -0,0 +1,374 @@
+<?
+/**
+ * Almanac ist die Klasse für das IPS-Modul 'IPSymconAlmanac'.
+ * Erweitert IPSModule 
+ */
+class Almanac extends IPSModule
+{
+  /**
+   * Bundeslaender IDs/Kuerzel - Array
+   *
+   * @access private
+   * @var array Key ist die id, Value ist der Kuerzel
+   */
+  static $States = array(
+    "1"   => "BY",  // Bayern
+    "2"   => "BW",  // Baden-Württemberg
+    "3"   => "NI",  // Niedersachsen
+    "4"   => "BE",  // Berlin
+    "5"   => "BB",  // Brandenburg
+    "6"   => "HB",  // Bremen
+    "7"   => "HH",  // Hamburg
+    "8"   => "HE",  // Hessen
+    "9"   => "MV",  // Mecklenburg-Vorpommern
+    "10"  => "NW",  // Nordrhein-Westfalen
+    "11"  => "RP",  // Rheinland-Pfalz
+    "12"  => "SL",  // Saarland
+    "13"  => "SN",  // Sachsen
+    "14"  => "ST",  // Sachsen-Anhalt
+    "15"  => "SH",  // Schleswig-Holstein
+    "16"  => "TH"   // Thüringen
+  );
+
+  /**
+   * Create.
+   *
+   * @access public
+   */
+  public function Create()
+  {
+    //Never delete this line!
+    parent::Create();
+
+    $this->RegisterPropertyString("State", "2");
+    $this->RegisterPropertyString("BaseURL", "https://www.schulferien.eu/downloads/ical4.php");
+		$this->RegisterPropertyBoolean("UpdateHoliday", true);
+		$this->RegisterPropertyBoolean("UpdateVacation", true);
+		$this->RegisterPropertyBoolean("UpdateDate", true);
+    // Update daily timer
+    $this->RegisterCyclicTimer("UpdateTimer", 0, 0, 1, 'ALMANAC_Update('.$this->InstanceID.');');
+  }
+
+  /**
+   * Apply Configuration Changes.
+   *
+   * @access public
+   */
+  public function ApplyChanges()
+  {
+    //Never delete this line!
+    parent::ApplyChanges();
+
+    $state = $this->ReadPropertyString("State");
+    $url  = $this->ReadPropertyString("BaseURL");
+    $holiday  = $this->ReadPropertyString("UpdateHoliday");
+    $vacation = $this->ReadPropertyString("UpdateVacation");
+    $date = $this->ReadPropertyString("UpdateDate");
+    $this->SendDebug("ApplyChanges", "federal state=".$state." (".$States[$state]."), url=".$url.", updates=".($holiday?'Y':'N')."|".($vacation?'Y':'N')."|".($date?'Y':'N'), 0);
+
+    $association =  Array(
+    	Array(0, "Nein", "Close", 0xFF0000),
+    	Array(1, "Ja",   "Ok", 0x00FF00)
+    );
+    $this->RegisterProfile(IPSVarType::vtBoolean, "ALMANAC.Question", "Bulb", "", "", 0, 0, 0, 0, $associations);
+
+    /*
+    $association =  Array(
+    	Array(1, "Montag", "", 0x0000FF),
+    	Array(2, "Dienstag", "", 0x0000FF1),
+    	Array(3, "Mittwoch", "", 0x0000FF),
+    	Array(4, "Donnerstag", "", 0x0000FF),
+    	Array(5, "Freitag", "", 0x0000FF),
+    	Array(6, "Samstag", "", 0x0000FF),
+    	Array(7, "Sonntag", "", 0x0000FF),
+    );
+    $this->RegisterProfile(IPSVarType::vtBoolean, "ALMANAC.Weekday", "Calendar", "", "", 0, 0, 0, 0, $associations);
+    */
+    
+    // Holiday
+    $this->RegisterVariable(IPSVarType::vtBoolean, "Ist Feiertag?", "IsHoliday",    "ALMANAC.Question", 1, !$holiday);
+    $this->RegisterVariable(IPSVarType::vtString, "Feiertag", "Holiday", "", 10, !$holiday);
+    // Urlaub
+    $this->RegisterVariable(IPSVarType::vtBoolean, "Ist Ferienzeit?", "IsVacation", "ALMANAC.Question", 2, !$vacation);
+    $this->RegisterVariable(IPSVarType::vtString, "Ferien", "Vacation", "", 20, !$vacation);
+    // Date
+    $this->RegisterVariable(IPSVarType::vtBoolean, "Ist Sommerzeit?", "IsSummer",   "ALMANAC.Question", 3, !$date);
+    $this->RegisterVariable(IPSVarType::vtBoolean, "Ist Schaltjahr?", "IsLeapyear", "ALMANAC.Question", 4, !$date);
+    $this->RegisterVariable(IPSVarType::vtBoolean, "Ist Wochenende?", "IsWeekend",  "ALMANAC.Question", 5, !$date);
+
+    $this->RegisterVariable(IPSVarType::vtInteger, "Kalenderwoche", "WeekNumber",  "", 30, !$date);
+    $this->RegisterVariable(IPSVarType::vtInteger, "Arbeitstage", "WorkDays",  "", 31, !$date);
+    $this->RegisterVariable(IPSVarType::vtInteger, "Tage im Monat", "DaysInMonth",  "", 32, !$date);
+    $this->RegisterVariable(IPSVarType::vtInteger, "Tage im Jahr",  "DaysInYear",  "", 33, !$date);
+  }
+
+  /**
+   * Create the profile for the given associations.
+   */
+	protected function RegisterProfile($vartype, $name, $icon, $prefix = "", $suffix = "", $minvalue = 0, $maxvalue = 0, $stepsize = 0, $digits = 0, $associations = NULL)
+	{
+		if (!IPS_VariableProfileExists($name))
+		{
+			switch ($vartype)
+			{
+				case IPSVarType::vtBoolean:
+					$this->RegisterProfileBoolean($name, $icon, $prefix, $suffix, $associations);
+					break;
+				case IPSVarType::vtInteger:
+					$this->RegisterProfileInteger($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $stepsize, $digits, $associations);
+					break;
+				case IPSVarType::vtFloat:
+					$this->RegisterProfileFloat($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $stepsize, $digits, $associations);
+					break;
+				case IPSVarType::vtString:
+					$this->RegisterProfileString($name, $icon);
+					break;
+			}	
+		}
+		return $name;
+	}
+
+	protected function RegisterProfileType($name, $type)
+	{
+    if(!IPS_VariableProfileExists($name)) {
+      IPS_CreateVariableProfile($name, $type);
+    } 
+    else {
+      $profile = IPS_GetVariableProfile($name);
+      if($profile['ProfileType'] != $type)
+        throw new Exception("Variable profile type does not match for profile ".$name);
+    }
+  }
+
+	protected function RegisterProfileBoolean($name, $icon, $prefix, $suffix, $asso)
+	{
+    RegisterProfileType($name, 0);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+	protected function RegisterProfileInteger($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $step, $digits, $asso)
+	{
+    RegisterProfileType($name, 1);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+    IPS_SetVariableProfileDigits($name, $digits);
+
+    if(sizeof($asso) === 0){
+      $minvalue = 0;
+      $maxvalue = 0;
+    } 
+    IPS_SetVariableProfileValues($name, $minvalue, $maxvalue, $step);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+	protected function RegisterProfileFloat($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $step, $digits, $asso)
+	{
+    RegisterProfileType($name, 2);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+    IPS_SetVariableProfileDigits($name, $digits);
+
+    if(sizeof($asso) === 0){
+      $minvalue = 0;
+      $maxvalue = 0;
+    } 
+    IPS_SetVariableProfileValues($name, $minvalue, $maxvalue, $step);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+	protected function RegisterProfileString($name, $icon, $prefix, $suffix)
+	{
+    RegisterProfileType($name, 3);
+
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+		IPS_SetVariableProfileIcon($name, $icon);
+  }
+
+
+  /**
+   * Create or delete variable.
+   */
+	protected function RegisterVariable($vartype, $name, $ident, $profile, $position, $delete)
+	{
+		if($delete == true) {
+			switch ($vartype) {
+				case IPSVarType::vtBoolean:
+					$objId = $this->RegisterVariableBoolean($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtInteger:
+					$objId = $this->RegisterVariableInteger($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtFloat:
+					$objId = $this->RegisterVariableFloat($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtString:
+					$objId = $this->RegisterVariableString($ident, $name, $profile, $position);
+					break;
+			}	
+		}
+		else {
+			$objId = @$this->GetIDForIdent($ident);
+			if ($objId > 0) {
+				$this->UnregisterVariable($ident);
+			}
+		}
+		
+		return $objId;
+	}
+  
+  /**
+   * Create the cyclic Update Timer.
+   *
+   * @access protected
+   * @param  string $ident Name and Ident of the Timer.
+   * @param  string $cId Client ID .
+   */
+  protected function RegisterCyclicTimer($ident, $hour, $minute, $second, $script)
+	{
+		$id = @$this->GetIDForIdent($ident);
+		$name = $ident;
+		if ($id && IPS_GetEvent($id)['EventType'] <> 1)
+		{
+		  IPS_DeleteEvent($id);
+		  $id = 0;
+		}
+		if (!$id)
+		{
+		  $id = IPS_CreateEvent(1);
+		  IPS_SetParent($id, $this->InstanceID);
+		  IPS_SetIdent($id, $ident);
+		}
+		IPS_SetName($id, $name);
+		// IPS_SetInfo($id, "Update AstroTimer");
+		// IPS_SetHidden($id, true);
+		IPS_SetEventScript($id, $script);
+		if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+		//IPS_SetEventCyclic($id, 0, 0, 0, 0, 0, 0);
+		IPS_SetEventCyclicTimeFrom($id, $hour, $minute, $second);
+		IPS_SetEventActive($id, false);
+	}
+  
+  /**
+   * Update a boolean value
+   * 
+   * @param string $Ident Ident of the boolean variable
+   * @param bool $value Value of the boolean variable
+   */
+  private function SetValueBoolean(string $ident, bool $value)
+  {
+    $id = $this->GetIDForIdent($ident);
+    SetValueBoolean($id, $value);
+  }
+
+  /**
+   * Update a string value
+   * 
+   * @param string $Ident Ident of the boolean variable
+   * @param string $value Value of the string variable
+   */
+  private function SetValueString(string $ident, string $value)
+  {
+    $id = $this->GetIDForIdent($ident);
+    SetValueString($id, $value);
+  }
+  
+
+  /**
+   * Holt den aktuellen Ferienkalender von http://www.schulferien.org und wertet diesen aus.
+   * Setzt den Namen der aktuellen Ferien oder 'Keine Ferien'.
+   *
+   * @access private
+   * @throws Exception Wenn Kalender nicht geladen werden konnte.
+   */
+  private function SetVacation($state, $url);
+  {
+    $year = date("Y");
+    $link = $url . '?land=' . $state . '&type=1&year=' . $year;
+    $this->SendDebug('GET', $link, 0);
+    $message = @file($link);
+    
+    if ($message === false) {
+      throw new Exception("Cannot load iCal Data.", E_USER_NOTICE);
+    }
+    
+    $this->SendDebug('LINES', count($message), 0);
+    $vacation = "Keine Ferien";
+    $count = (count($message) - 1);
+    
+    for ($line = 0; $line < $count; $line++) {
+      if (strstr($message[$line], "SUMMARY:")) {
+        $name   = trim(substr($message[$line], 8));
+        $start  = trim(substr($message[$line + 1], 19));
+        $end    = trim(substr($message[$line + 2], 17));
+        $this->SendDebug("MESSAGE", "SUMMARY: ".$name." ,START: ".$start." ,END: ".$end, 0);
+        $now = date("Ymd") . "\n";
+        if (($now >= $start) and ( $now <= $end)) {
+          $vacation = explode(' ', $name)[0];
+          $this->SendDebug('FOUND', $vacation 0);
+        }
+      }
+    }
+    
+    $this->SetValueString("Holiday", $holiday);
+    $this->SetValueBoolean("IsHoliday", ($holiday == "Keine Ferien")? false:true);
+  }
+
+  /**
+  * This function will be available automatically after the module is imported with the module control.
+  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+  *
+  * ALMANAC_Update($id);
+  *
+  */
+  public function Update()
+  {
+    $state = $this->ReadPropertyString("State");
+    $url  = $this->ReadPropertyString("BaseURL");
+    $holiday  = $this->ReadPropertyString("UpdateHoliday");
+    $vacation = $this->ReadPropertyString("UpdateVacation");
+    $date = $this->ReadPropertyString("UpdateDate");
+
+    if($holiday == true) {
+        try {
+            $this->SetHoliday($state, $url);
+        }
+        catch (Exception $exc) {
+            trigger_error($exc->getMessage(), $exc->getCode());
+            $this->SendDebug('ERROR HOLIDAY', $exc->getMessage(), 0);
+        }
+    }
+    if($vacation == true) {
+        try {
+            $this->SetVacation($state, $url);
+        }
+        catch (Exception $exc) {
+            trigger_error($exc->getMessage(), $exc->getCode());
+            $this->SendDebug('ERROR VACATION', $exc->getMessage(), 0);
+        }
+    }
+    if($date == true) {
+      $this->SetDate($state, $url);
+    }
+  }
+}
+?>
---
+++ README.md
@@ -0,0 +1,107 @@
+# Almanac
+
+Dieses Modul bietet Kalenderinformationen für Arbeitstage, Feiertage, Schulferien und andere Kalenderdaten.
+
+### Danksagung 
+
+Dieses Modul basiert auf den Ideen und Modulen von ...
+* _Nall-chan_ : Modul _Schulferien_ (https://github.com/Nall-chan/IPSSchoolHolidays) 
+
+Vielen Dank für die hervorragende und tolle Arbeit! 
+
+### Inhaltverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Software-Installation](#3-software-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#5-statusvariablen-und-profile)
+6. [WebFront](#6-webfront)
+7. [PHP-Befehlsreferenz](#7-php-befehlsreferenz)
+8. [Lizenz](#8-lizenz)
+
+### 1. Funktionsumfang
+
+Das Modul nutzt die von schulferien.eu (www.schulferien.eu) bereitgestellten Daten zur Anzeige der Feiertage und Schulferien
+für das gewählte Bundesland.  
+Darüber hinaus werden mittels der PHP Funktion "date" verschiedene Informationen für das aktuelle Datum ermittelt
+
+Folgende Informationen werden ermittelt:
+
+* Sind Ferien und welche
+* Feiertag oder nicht und wie heißt er 
+* Anzahl Tage im Jahr
+* Anzahl Tage im Monat
+* Arbeitstage im Monat
+* Schaltjahr oder nicht
+* Sommerzeit oder nicht
+* Wochenende oder nicht
+* Nummer der Kalenderwoche
+
+### 2. Voraussetzungen
+
+- IP-Symcon ab Version 4.x (getestet mit Version 4.4 auf RP3)
+
+### 3. Software-Installation
+
+Über das Modul-Control folgende URL hinzufügen.  
+`https://github.com/Wilkware/IPSymconAlmanac` oder `git://github.com/Wilkware/IPSymconAlmanac.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+- Unter "Instanz hinzufügen" ist das 'Almanac'-Modul (Alias: Kalender, Schulferien, Feiertage) unter dem Hersteller '(Sonstige)' aufgeführt.
+
+__Konfigurationsseite__:
+
+
+Name               | Beschreibung
+------------------ | ---------------------------------
+Bundesland         | Auswahl des Bundesland für welchen man die Feiertage und Schulferien ermittelt haben möchte.
+Basis URL          | Url zum Dienstanbieter für Feiertage und Schulferien, derzeit 'https://www.schulferien.eu/downloads/ical4.php'
+Feiertage          | Status, ob Ermittlung der Feiertage erwünscht ist.
+Schulferien        | Status, ob Ermittlung der Schulferien erwünscht ist.
+Datumsfunktion     | Status, ob Informationen zum aktuellen Datum erwünscht sind.
+
+
+### 5. Statusvariablen und Profile
+
+Die Statusvariablen/Timer werden automatisch angelegt. Das Löschen einzelner kann zu Fehlfunktionen führen.
+
+Name                 | Typ       | Beschreibung
+-------------------- | --------- | ----------------
+Feiertag             | String    | Name des Feriertages oder 'kein Feiertag'
+Ist Feiertag         | Boolean   | Ist aktueller Tag ein Feiertag?
+Ferien               | String    | Name der Schulferien oder 'keine Ferien'
+Ist Ferienzeit       | Boolean   | Fällt aktueller Tag in die Ferien?
+Tage im Jahr         | Integer   | Wieviel Tage hat das aktuelle Jahr?
+Tage im Monat        | Integer   | Wieviel Tage hat der aktuelle Monat?
+Arbeitstage          | Integer   | Wieviel Arbeitstage hat der aktuelle Monat?
+Kalenderwoche        | Integer   | Nummer der aktuelle Kalenderwoche
+Ist Schaltjahr       | Boolean   | Ist aktueller Jahr ein Schaltjahr?
+Ist Sommerzeit       | Boolean   | Ist aktuell Sommerzeit aktiv?
+Ist Wochenende       | Boolean   | Ist gerade Wochenende?
+
+Folgende Profile werden angelegt:
+
+Name                 | Typ       | Beschreibung
+-------------------- | --------- | ----------------
+ALMANAC.Question     | Boolean   | FALSE = Nein / TRUE = Ja
+
+
+### 6. WebFront
+
+Man kann die Statusvariaben(Strings) direkt im WF verlinken.
+
+
+### 7. PHP-Befehlsreferenz
+
+`void ALMANAC_Update(int $InstanzID);`
+Holt entsprechend der Konfiguration die gewählten Daten.
+Die Funktion liefert keinerlei Rückgabewert.
+
+Beispiel:
+`ALMANAC_Update(12345);`
+
+### 8. Lizenz
+
+  [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)  
